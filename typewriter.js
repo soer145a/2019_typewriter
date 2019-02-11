ab446e575707d10bad704adbdae63f03aa7c6cb9
soer145a <36399126+soer145a@users.noreply.github.com>
2019-02-11 15:24:34 +0100

    Add files via upload

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,44 @@
 "use strict";
+window.addEventListener("DOMContentLoaded", init);
+let counter;
+let myText;
+let slicedText;
+let myTextL;
+
+let audioDecider;
+
+let audioSpace = new Audio("typespace.mp3");
+let audio1 = new Audio("typekey1.mp3");
+let audio2 = new Audio("typekey2.mp3");
+
+function init() {
+  myText = document.getElementById("typewriter").textContent;
+
+  myTextL = myText.length;
+  counter = 0;
+
+  document.getElementById("typewriter").textContent = "";
+  loop();
+}
+function loop() {
+  slicedText = myText.slice(0, counter + 1);
+  audioDecider = slicedText.slice(counter);
+  counter++;
+
+  if (audioDecider == " ") {
+    audioSpace.play();
+  } else {
+    audio1.play();
+  }
+  console.log(audioDecider);
+
+  document.getElementById("typewriter").textContent = slicedText;
+  if (counter != myTextL) {
+    setTimeout(loop, 400);
+  } else {
+    end();
+  }
+}
+function end() {
+  console.log("end");
+}
